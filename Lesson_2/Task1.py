c51626e769f2d2b302eeeb783b45658d5ea2ca73
Kavkaz887 <aaartemb887@gmail.com>
2023-04-06 21:04:45 +0300

    add file

---
+++ Lesson_2/Task1.py
@@ -0,0 +1,7 @@
+list = input("Enter your list: ").split(" ")
+search_element = input("Enter element: ")
+counter = 0
+for element in list:
+    if search_element == element:
+        counter += 1
+print(counter)
